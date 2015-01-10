0bec5130b020abd9ae03d01fb43bd31fe0488daa
Felix Mulder <felix.mulder@gmail.com>
2015-01-10 18:23:22 +0100

    convert questions.py to function stuff

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 #!/bin/env python
 import random
 from flask import Flask
-from questions import questions
+from questions import get_questions
 from flask import render_template
 
+questions = get_questions()
 app = Flask(__name__)
 
 @app.route("/")
 def root():
+    global questions
     q_iter = [(k, v) for k, v in questions.items()]
 
     if len(q_iter) == 0:
+        questions = get_questions()
         return "YOU ARE WINNER!"
 
     rand = random.randint(0, len(q_iter)-1)
